89b2320153fda56cc47240faddcf07bdc09e7b5c
Alan Wang <alan.wang@bcgdv.com>
2020-04-19 23:44:21 +0800

    clean up

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,7 +16,7 @@ function main() {
   login
   docker_build $INPUT_TAGS $ACCOUNT_URL
   create_ecr_repo $INPUT_CREATE_REPO
-  docker_push_to_ecr $INPUT_TAGS $ACCOUNT_URL
+  docker_push_to_ecr $INPUT_TAGS $ACCOUNT_URL".cn"
 }
 
 function sanitize() {
